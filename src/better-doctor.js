e2edb02ae5f9dc10eec4178a578a719eceb92b27
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-01 16:31:34 -0700

    add name as element to query in order to search according to doctor name

--- src/better-doctor.js
+++ src/better-doctor.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
 var Promise = require('es6-promise').Promise;
 
-export function findDoctors(issue) {
+export function findDoctors(issue, name) {
 
   return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
     console.log(url);
 
     request.onload = function() {
--- src/main.js
+++ src/main.js
@@ -11,16 +11,23 @@ $(document).ready(function() {
 
     let issue = $('#issue').val();
     $('#issue').val("");
+    let name = $('#name').val();
+    $('#name').val("");
 
-    findDoctors(issue)
+    findDoctors(issue, name)
 
     .then(function(response) {
       let body = JSON.parse(response);
-      // var counter = 0;
-      console.log(body.data[1].practices[3].name);
-        // body.data.forEach(function(doctor){
-          $('.showOutput').text(`The doctors are ${body.data[1].practices[3].name}`);
-        // });
+      $('.showOutput').empty();
+      var counter = 0;
+        body.data.forEach(function(doctor){
+          $('.showOutput').append('<li>' + `The doctors name is: ${doctor.practices[0].name}, address: ${doctor.practices[0].visit_address.street}, ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.zip}, phone number: ${doctor.practices[0].phones[0].number}, website: ${doctor.practices[0].website}. Does this doctor accept new patients: ${doctor.practices[0].accepts_new_patients}` + '</li>');
+
+          counter++;
+        });
+        if (counter == 0) {
+          $('.showOutput').append("According to input criteria no doctors found! Try again with a different input");
+        }
     }, function(error) {
       $('.showErrors').append('<li>' + `There was an error processing your request: ${error.message}` + '</li>');
     });
